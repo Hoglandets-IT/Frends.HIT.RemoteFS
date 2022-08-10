24def7896fcab73b3d0700f7a72666ae4afca58f
Lars Scheibling <lars.scheibling@hoglandet.se>
2022-08-10 17:05:58 +0200

    Added build action

---
+++ quickbuild.sh
@@ -0,0 +1,15 @@
+VERSION=$(cat ./VERSION)
+APIKEY=$1
+# Increment version number
+IFS=. read -r v1 v2 v3 <<< "$VERSION"
+v3=$((v3 + 1))
+VERSION="$v1.$v2.$v3"
+
+echo "Building version $VERSION" &&
+rm -f ./Frends.HIT.RemoteFS.*.nupkg &&
+dotnet pack --configuration Release --include-source --output . /p:Version=$VERSION Frends.HIT.RemoteFS/Frends.HIT.RemoteFS.csproj &&
+dotnet nuget push "Frends.HIT.RemoteFS.$VERSION.nupkg" --source https://proget.intern.hoglandet.se/nuget/Frends/ --api-key "$APIKEY" &&
+echo "Done" &&
+echo "$VERSION" > ./VERSION
+
+rm -f ./Frends.HIT.RemoteFS.*.nupkg
