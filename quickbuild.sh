92f835312086e0d0b8507dda81d815a6fa730ef2
Lars Scheibling <lars.scheibling@hoglandet.se>
2022-08-10 17:11:27 +0200

    Updated workflow

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 VERSION=$(cat ./VERSION)
 APIKEY=$1
 # Increment version number
@@ -8,7 +9,7 @@ VERSION="$v1.$v2.$v3"
 echo "Building version $VERSION" &&
 rm -f ./Frends.HIT.RemoteFS.*.nupkg &&
 dotnet pack --configuration Release --include-source --output . /p:Version=$VERSION Frends.HIT.RemoteFS/Frends.HIT.RemoteFS.csproj &&
-dotnet nuget push "Frends.HIT.RemoteFS.$VERSION.nupkg" --source https://proget.intern.hoglandet.se/nuget/Frends/ --api-key "$APIKEY" &&
+dotnet nuget push "Frends.HIT.RemoteFS.$VERSION.nupkg" --source https://proget.hoglandet.se/nuget/Frends/ --api-key "$APIKEY" &&
 echo "Done" &&
 echo "$VERSION" > ./VERSION
 
