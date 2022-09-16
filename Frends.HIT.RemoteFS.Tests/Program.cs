019b4e6f46d41347bf9496bcafa9260df4dbb153
Lars Scheibling <lars@scheibling.se>
2022-09-16 07:55:02 +0000

    Added tests

---
+++ Frends.HIT.RemoteFS.Tests/Program.cs
@@ -0,0 +1,33 @@
+﻿using Frends.HIT.RemoteFS;
+
+using EzSmb;
+using EzSmb.Paths;
+
+string testServer = "";
+string testDomain = "";
+string testUser = "";
+string testPassword = "";
+string testShare = "";
+
+
+
+var par = new ListParams();
+par.Path = testShare;
+par.Filter = FilterTypes.None;
+par.Pattern = "";
+
+var conn = new ServerConfiguration(
+    connectiontype: ConnectionTypes.SMB, 
+    address: testServer,
+    domain: testDomain,
+    username: testUser,
+    password: testPassword,
+    privatekey: "",
+    privatekeypassword: "",
+    fingerprint: ""
+);
+
+var files = await SMB.ListFiles(par, conn);
+foreach (var fle in files) {
+    Console.WriteLine(fle);
+}
